aa4b570198dbfe76733a8a8e7d7e1857b8ffa70a
Bryce Kalmbach <jbkalmbach@gmail.com>
2018-02-15 14:33:20 -0800

    Using Gaussians and vectorization to speed things up.

--- siggi/calcIG.py
+++ siggi/calcIG.py
@@ -2,7 +2,8 @@ from __future__ import division
 
 import numpy as np
 from sklearn.neighbors.kde import KernelDensity
-from . import Sed, Bandpass
+from scipy import stats
+from . import Sed, Bandpass, PhotometricParameters
 __all__ = ["calcIG"]
 
 
@@ -14,7 +15,7 @@ class calcIG(object):
     """
 
     def __init__(self, filter_dict, sed_list, prior_func,
-                 y_min, y_max, y_step):
+                 y_min, y_max, y_step, snr=5.):
 
         self._filter_dict = filter_dict
         self._sed_list = []
@@ -26,12 +27,12 @@ class calcIG(object):
             sed_copy.flambda[np.where(np.isnan(sed_copy.flambda))] = 0.
             imsimBand = Bandpass()
             imsimBand.imsimBandpass()
-            f_norm = sed_copy.calcFluxNorm(-10.0, imsimBand)
+            f_norm = sed_copy.calcFluxNorm(18.0, imsimBand)
             sed_copy.multiplyFluxNorm(f_norm)
             self._sed_list.append(sed_copy)
         self._flat_sed = Sed()
         self._flat_sed.setFlatSED(wavelen_min=99., wavelen_max=1500.)
-        f_norm = self._flat_sed.calcFluxNorm(-15.0, imsimBand)
+        f_norm = self._flat_sed.calcFluxNorm(18.0, imsimBand)
         self._flat_sed.multiplyFluxNorm(f_norm)
         self._flat_sed.resampleSED(wavelen_match=self._sed_list[0].wavelen)
 
@@ -41,74 +42,28 @@ class calcIG(object):
         self.y_max = y_max
         self.y_step = y_step
 
+        self.snr = snr
+
         return
 
-    def draw_colors(self, num_points, random_seed=17):
+    def calc_colors(self):
 
-        np.random.seed(random_seed)
+        sed_colors = []
+        color_errors = []
 
-        mags_list = []
-        color_list = []
-        true_sed_list = []
-
-        sed_on = 0
-        sed_copy_1 = Sed()
-
-        y_range = np.arange(self.y_min,
-                            self.y_max + 0.5*self.y_step,
-                            self.y_step)
-        sed_probs = self.prior(y_range)
-        sed_choices = np.random.choice(y_range, p=sed_probs, 
-                                       size=num_points)
-        bins = np.arange(self.y_min - 0.5*self.y_step,
-                         self.y_max + 1.5*self.y_step,
-                         self.y_step)
-        bin_counts, _ = np.histogram(sed_choices, bins=bins)
+        mag_error = 1.0/self.snr
+        color_error = np.sqrt(2.)*mag_error  # Just error prop for sum
 
         for sed_obj in self._sed_list:
 
-            sed_copy_1.setSED(wavelen=sed_obj.wavelen, 
-                              flambda=sed_obj.flambda)
+            sed_mags = self._filter_dict.magListForSed(sed_obj)
+            if np.isnan(sed_mags[0]):
+                print(sed_mags)
+            sed_colors.append([sed_mags[i] - sed_mags[i-1]
+                               for i in range(len(sed_mags) - 1)])
+            color_errors.append([color_error])
 
-            for i in range(bin_counts[sed_on]):
-                #  TODO: Add realistic errors
-
-                err_val = np.random.normal(loc=0.0, 
-                                           scale=np.sqrt(sed_obj.flambda))
-                sed_copy_1.flambda = sed_obj.flambda + .4*err_val
-                sed_copy_1.flambda += self._flat_sed.flambda
-                sed_copy_1.flambda[sed_copy_1.flambda <= 0.] = 0.
-                flux_with_errors = self._filter_dict.fluxListForSed(sed_copy_1)
-                mags = [sed_copy_1.magFromFlux(f) for f in flux_with_errors]
-                mags_list.append(mags)
-                color_list.append([mags[i] - mags[i-1] 
-                                   for i in range(len(mags)-1)])
-                true_sed_list.append(sed_on)
-            sed_on += 1
-
-        return np.array(mags_list), np.array(color_list), \
-            np.array(true_sed_list)
-
-    def calc_density(self, colors, truth_vals, bandwidth=None):
-
-        if bandwidth is None:
-            bandwidth = 0.05  # TODO: Fix this to adjust based on color range
-
-        kde_total = KernelDensity(kernel='gaussian',
-                                  bandwidth=bandwidth)
-        kde_total.fit(colors)
-
-        kde_list = []
-
-        for y_i in np.unique(truth_vals):
-            kde_1 = KernelDensity(kernel='gaussian',
-                                  bandwidth=bandwidth)
-            kde_1.fit(colors[np.where(truth_vals == y_i)])
-            kde_list.append(kde_1)
-
-        kde_lists = [kde_total, kde_list]
-
-        return kde_lists
+        return np.array(sed_colors), np.array(color_errors)
 
     def calc_h(self):
 
@@ -125,31 +80,45 @@ class calcIG(object):
 
         return h_sum
 
-    def calc_hyx(self, kde_lists):
+    def calc_hyx(self, colors, errors):
+
+        y_range = np.arange(self.y_min,
+                            self.y_max + 0.5*self.y_step,
+                            self.y_step)
+        sed_probs = self.prior(y_range)
 
         hyx_sum = 0
-        total_kde, y_kde_list = kde_lists
-        num_y = len(y_kde_list)
-        dens_step = 0.01
-        dens_range = np.arange(-2., 2., dens_step).reshape(-1,1)
-        total_dens = np.exp(total_kde.score_samples(dens_range))
+        c_max = np.max(colors) + 5*np.max(errors)
+        c_min = np.min(colors) - 5*np.max(errors)
+        dens_step = (c_max - c_min) / 250.
 
-        for idx in range(num_y):
-            y_dens = np.exp(y_kde_list[idx].score_samples(dens_range))
-            for x_val, yx_val in zip(total_dens, y_dens):
-                p_x_y = yx_val*dens_step/num_y
-                p_x = x_val*dens_step
-                if ((p_x_y > 0.) & (p_x > 0.)):
-                    hyx_sum += p_x_y*np.log2(p_x_y/p_x)
+        dens_range = np.arange(c_min, c_max, dens_step)
+
+        rv = stats.norm
+
+        x_total = np.zeros(len(dens_range))
+        y_vals = []
+
+        for idx in range(len(y_range)):
+            y_dens = sed_probs[idx]*rv.pdf(dens_range, colors[idx],
+                                           errors[idx])
+            x_total += y_dens
+            y_vals.append(y_dens)
+
+        y_vals = np.array(y_vals)
+
+        for idx in range(len(dens_range)):
+            hyx_i = np.nansum(dens_step*y_vals[:, idx]*np.log2(y_vals[:, idx] /
+                                                               x_total[idx]))
+            hyx_sum += hyx_i
 
         return -1.*hyx_sum
 
     def calc_IG(self):
 
-        mags, colors, truth = self.draw_colors(150*len(self._sed_list))
-        k = self.calc_density(colors, truth)
+        colors, errors = self.calc_colors()
         hy_sum = self.calc_h()
-        hyx_sum = self.calc_hyx(k)
+        hyx_sum = self.calc_hyx(colors, errors)
 
         info_gain = hy_sum - hyx_sum
 
--- siggi/filters.py
+++ siggi/filters.py
@@ -52,22 +52,24 @@ class filters(object):
 
             offset = self.wavelen_step/2.
 
-            wavelen_arr = np.arange(self.wavelen_min, 
+            wavelen_arr = np.arange(self.wavelen_min,
                                     self.wavelen_max+offset,
                                     self.wavelen_step)
 
             sb = np.zeros(len(wavelen_arr))
 
             climb_width = (band[1] - band[2])/2.
-            ### TODO: Fix this for top hat filter because fails when climb_width=0.
+            # TODO: Fix this for top hat filter which fails when climb_width=0.
             slope = 1./climb_width
-            climb_values = np.array([slope*i for i in \
-                                     np.arange(0, climb_width+offset, 
+            climb_values = np.array([slope*i for i in
+                                     np.arange(0, climb_width+offset,
                                                self.wavelen_step)])
-            
+
             climb_steps = len(climb_values)
-            min_idx = np.where(wavelen_arr >= (band[0]-(band[1]/2.)-offset))[0][0]
-            max_idx = np.where(wavelen_arr >= (band[0]+(band[1]/2.)+offset))[0][0]
+            min_idx = np.where(wavelen_arr >=
+                               (band[0]-(band[1]/2.)-offset))[0][0]
+            max_idx = np.where(wavelen_arr >=
+                               (band[0]+(band[1]/2.)+offset))[0][0]
             sb[min_idx:max_idx] = 1.0
             sb[min_idx:min_idx+climb_steps] = climb_values
             sb[max_idx-climb_steps:max_idx] = 1. - climb_values
@@ -75,7 +77,8 @@ class filters(object):
             bp_object = Bandpass(wavelen=wavelen_arr, sb=sb)
 
             bandpass_list.append(bp_object)
-            name_list = ['filter_%i' % idx for idx in range(len(bandpass_list))]
+            name_list = ['filter_%i' % idx for idx in
+                         range(len(bandpass_list))]
 
             bandpass_dict = BandpassDict(bandpass_list, name_list)
 
