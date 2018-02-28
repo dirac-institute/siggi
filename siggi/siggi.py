a1d360e97c24c2b451cef021a828d42a5d3d4f8f
Bryce Kalmbach <jbkalmbach@gmail.com>
2018-02-27 20:46:32 -0800

    Added multiprocessing.

--- siggi/siggi.py
+++ siggi/siggi.py
@@ -1,7 +1,11 @@
 import numpy as np
+import multiprocessing as mp
 from copy import deepcopy
 from functools import reduce
 from . import filters, spectra, calcIG
+
+def unwrap_self_f(arg, **kwarg):
+    return siggi.grid_results(*arg, **kwarg)
 
 
 class siggi(object):
@@ -31,36 +35,58 @@ class siggi(object):
                          width_steps=10, adjust_width_ratio=False, 
                          ratio_min=0.1, ratio_max=1.0, ratio_steps=10):
 
-        filt_wave_range = np.linspace(filt_min, filt_max, filt_steps)
+        self.filt_wave_range = np.linspace(filt_min, filt_max, filt_steps)
 
-        dim_list = [len(filt_wave_range) for i in range(num_filters)]
+        dim_list = [len(self.filt_wave_range) for i in range(num_filters)]
         if adjust_widths is True:
             dim_list.insert(0, width_steps)
         if adjust_width_ratio is True:
             dim_list.insert(0, ratio_steps)
-        result_grid = np.zeros(dim_list)
-        num_gridpoints = reduce((lambda x, y: x*y), dim_list)
 
-        f = filters(filt_min-width_max, filt_max+width_max)
+        num_points = reduce((lambda x, y: x*y), dim_list)
 
         step_on = 0
 
-        for idx in range(num_gridpoints):
+        processes = []
 
-            step_indices = np.unravel_index(idx, dim_list)
-            if step_indices[0] == step_on:
-                print(step_on)
-                step_on += 1
+        pool = mp.Pool(processes=4)
 
-            filt_centers = [filt_wave_range[filt_idx] 
-                            for filt_idx in step_indices]
-            filt_dict = f.trap_filters([[filt_loc, 120, 60]
-                                        for filt_loc in filt_centers])
+        pool_res = pool.map(unwrap_self_f, zip([self]*num_points, 
+                                               [[idx, dim_list, width_max,
+                                                 snr_level]
+                                                for idx in range(num_points)]))
 
-            c = calcIG(filt_dict, self.shift_seds, self.z_prior, self.z_min,
-                       self.z_max, self.z_steps, snr=snr_level)
-            result_grid[step_indices] = c.calc_IG()
-
-#print(step_indices, filt_centers, result_grid[step_indices])
+        result_grid = np.reshape(pool_res, dim_list)
 
         return result_grid
+
+    def grid_results(self, arg_list):
+
+        idx = arg_list[0]
+        dim_list = arg_list[1]
+        width_max = arg_list[2]
+        snr_level = arg_list[3]
+
+        step_indices = np.unravel_index(idx, dim_list)
+
+        filt_centers = [self.filt_wave_range[filt_idx] 
+                        for filt_idx in step_indices]
+
+        filt_diffs = [filt_centers[idx] - filt_centers[idx-1] 
+                    for idx in range(1, len(filt_centers))]
+        filt_diffs = np.array(filt_diffs, dtype=np.int)
+        
+        if np.min(filt_diffs) < 0:
+            return 0.
+
+        f = filters(self.filt_wave_range[0] - width_max,
+                    self.filt_wave_range[-1] + width_max)
+
+        filt_dict = f.trap_filters([[filt_loc, 120, 60]
+                                    for filt_loc in filt_centers])
+
+        c = calcIG(filt_dict, self.shift_seds, self.z_prior, self.z_min,
+                   self.z_max, self.z_steps, snr=snr_level)
+        step_result = c.calc_IG()
+
+        return step_result
