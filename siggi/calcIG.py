3474ddabff9a5de2a07c711e30416ea09e6330ae
Bryce Kalmbach <jbkalmbach@gmail.com>
2018-03-13 03:08:10 -0700

    Lowered default to 5000 for now.

--- siggi/calcIG.py
+++ siggi/calcIG.py
@@ -88,7 +88,7 @@ class calcIG(object):
 
         y_vals = []
         y_distances = []
-        num_points = 10000
+        num_points = 5000
         x_total = np.zeros((num_seds*num_points, num_colors))
 
         for idx in range(num_seds):
