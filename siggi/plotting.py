60dec8ed1490ea8ad19d41578beb03301d8026b0
Bryce Kalmbach <jbkalmbach@gmail.com>
2018-04-19 01:46:47 -0700

    Plotting routine added.

--- siggi/plotting.py
+++ siggi/plotting.py
@@ -5,6 +5,7 @@ from .lsst_utils import BandpassDict
 
 __all__ = ["plotting"]
 
+
 class plotting(object):
 
     def __init__(self, sed_list, best_point, width, ratio):
@@ -36,20 +37,25 @@ class plotting(object):
         self.filter_dict = filter_dict
         self.sed_list = sed_list
 
-    def plot_filters(self):
+    def plot_filters(self, fig=None):
 
-        fig = plt.figure(figsize=(12, 6))
+        if fig is None:
+            fig = plt.figure(figsize=(12, 6))
 
         for sed_obj in self.sed_list:
-            plt.plot(sed_obj.wavelen, sed_obj.flambda/np.max(sed_obj.flambda), c='k', alpha=0.5)
+            plt.plot(sed_obj.wavelen, sed_obj.flambda/np.max(sed_obj.flambda),
+                     c='k', alpha=0.5)
 
-        c_list = ['r', 'orange', 'yellow', 'green', 'blue', 'violet']
+        c_list = np.linspace(0, 1, len(self.filter_dict.values()))
+
+        cmap = plt.get_cmap('rainbow')
 
         for filt, color in zip(self.filter_dict.values(), c_list):
-            plt.fill(filt.wavelen, filt.sb, c=color, zorder=10, alpha=0.6)
-        plt.xlim(249., 1251.)
+            plt.fill(filt.wavelen, filt.sb,
+                     c=cmap(color),
+                     zorder=10, alpha=0.6)
+        plt.xlim(filt.wavelen[0] - 50., filt.wavelen[-1] + 50)
         plt.xlabel('Wavelength (nm)')
         plt.ylabel('Scaled Flux')
-        plt.title('Information Gain Test z <= 2')
 
-        plt.savefig('test.png')
+        return fig
