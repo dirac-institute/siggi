Added test for different scaling in integration. Found error! Fixed math error.

siggi/calcIG.py
@@ -142,7 +142,10 @@ class calcIG(integrationUtils):
 
             y_samples = y_samples.reshape(num_points, num_colors)
 
-            y_dist = cdist(y_samples, [colors[idx]]).flatten()
+            inv_cov = np.linalg.inv(np.diagflat(errors[idx]**2.))
+
+            y_dist = cdist(y_samples, [colors[idx]], metric='mahalanobis',
+                           VI=inv_cov).flatten()
             y_sort = np.argsort(y_dist)
             y_dist = y_dist[y_sort]
             y_samples = y_samples[y_sort]

tests/test_mathUtils.py
@@ -10,7 +10,7 @@ from siggi.mathUtils import integrationUtils
 
 class testMathUtils(unittest.TestCase):
 
-    def test_integrationUtils(self):
+    def test_integrationUtils_same_scale(self):
 
         gauss2d = stats.multivariate_normal
         num_pts = 20000
@@ -31,6 +31,34 @@ class testMathUtils(unittest.TestCase):
                                                     np.ones(2))
         y_values = gauss2d.pdf(y_samples, mean=np.ones(2),
                                cov=np.diagflat(np.ones(2))**2.)
+        integral_sum = np.nansum(y_values*norm_factor)
+
+        np.testing.assert_almost_equal(integral_sum, 1.0, decimal=3)
+
+    def test_integrationUtils_diff_scale(self):
+
+        gauss2d = stats.multivariate_normal
+        num_pts = 20000
+
+        test_pts = gauss2d.rvs(mean=np.ones(2),
+                               cov=np.diagflat(np.array([.3, .5])**2.),
+                               size=num_pts)
+
+        y_samples = test_pts.reshape(num_pts, 2)
+
+        inv_cov = np.linalg.inv(np.diagflat(np.array([.3, .5])**2.))
+        y_dist = cdist(y_samples, np.ones(2).reshape(1, 2),
+                       metric='mahalanobis',
+                       VI=inv_cov).flatten()
+        y_sort = np.argsort(y_dist)
+        y_dist = y_dist[y_sort]
+        y_samples = y_samples[y_sort]
+
+        i_utils = integrationUtils()
+        norm_factor = i_utils.calc_integral_scaling(y_dist, 2,
+                                                    np.array([.3, .5]))
+        y_values = gauss2d.pdf(y_samples, mean=np.ones(2),
+                               cov=np.diagflat(np.array([.3, .5])**2.))
         integral_sum = np.nansum(y_values*norm_factor)
 
         np.testing.assert_almost_equal(integral_sum, 1.0, decimal=3)