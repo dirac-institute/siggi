c68183a876303a0821e66938e705f1dc79943e17
Bryce Kalmbach <jbkalmbach@gmail.com>
2018-08-03 12:30:07 -0700

    Added documentation and more tests for integration.

--- siggi/mathUtils.py
+++ siggi/mathUtils.py
@@ -12,13 +12,30 @@ class integrationUtils(object):
 
     def calc_integral_scaling(self, distances, n_dim, dim_scale):
 
-        # Volume of hyperellipsoid: https://math.stackexchange.com/
-        # questions/332391/volume-of-hyperellipsoid
+        """
+        The integration works by calculating the volume of
+        slices of a hyperellipsoid under the probability density
+        curve.
 
+        This part calculates the area of each slice and when
+        it is returned it is multiplied by the pdf value
+        to get a volume.
+
+        Since the width of the probability distribution
+        in each dimension will be different this scales
+        each dimension in the integral appropriately.
+        """
+
+        # (r_outer ^ d) - (r_inner ^ d)
         norm_factor = (distances[1:]**n_dim -
                        distances[:-1]**n_dim)
         norm_factor = np.append((distances[0]**n_dim),
                                 norm_factor)
+
+        # Volume of hyperellipsoid: https://math.stackexchange.com/
+        # questions/332391/volume-of-hyperellipsoid with V_d term
+        # from wikipedia: https://en.wikipedia.org/wiki/N-sphere
+
         norm_factor *= ((np.pi**(n_dim/2.))/gamma((n_dim/2.)+1))
         norm_factor *= np.linalg.det(np.diag(dim_scale))
 
--- tests/test_mathUtils.py
+++ tests/test_mathUtils.py
@@ -10,6 +10,62 @@ from siggi.mathUtils import integrationUtils
 
 class testMathUtils(unittest.TestCase):
 
+    def test_integral_scaling(self):
+
+        y_distances = np.array([0.5, 1.0, 1.5])
+
+        i_utils = integrationUtils()
+
+        # Since we use absolute differences from zero and the unit
+        # 1-ball is on the interval [-1, 1] the truth in 1-d is double
+        # difference
+        truth_1d = np.array([0.5, 0.5, 0.5])*2
+
+        i_out_1d = i_utils.calc_integral_scaling(y_distances,
+                                                 1, [1.0])
+
+        np.testing.assert_array_almost_equal(truth_1d, i_out_1d)
+
+        truth_2d = [np.pi*(0.5**2),
+                    np.pi*(1.0**2 - 0.5**2),
+                    np.pi*(1.5**2 - 1.0**2)]
+
+        i_out_2d = i_utils.calc_integral_scaling(y_distances,
+                                                 2, np.ones(2))
+
+        np.testing.assert_array_almost_equal(truth_2d, i_out_2d)
+
+        truth_3d = [(4./3.)*np.pi*(0.5**3),
+                    (4./3.)*np.pi*(1.0**3 - 0.5**3),
+                    (4./3.)*np.pi*(1.5**3 - 1.0**3)]
+
+        i_out_3d = i_utils.calc_integral_scaling(y_distances,
+                                                 3, np.ones(3))
+
+        np.testing.assert_array_almost_equal(truth_3d, i_out_3d)
+
+    def test_integration_1d(self):
+
+        gauss = stats.multivariate_normal
+        num_pts = 20000
+
+        test_pts = gauss.rvs(mean=1.0, cov=[1.0], size=num_pts)
+
+        y_samples = test_pts.reshape(num_pts, 1)
+
+        y_dist = cdist(y_samples, [[1.0]]).flatten()
+        y_sort = np.argsort(y_dist)
+        y_dist = y_dist[y_sort]
+        y_samples = y_samples[y_sort]
+
+        i_utils = integrationUtils()
+        norm_factor = i_utils.calc_integral_scaling(y_dist, 1, [1.0])
+
+        y_values = gauss.pdf(y_samples, mean=1.0, cov=[1.0])
+        integral_sum = np.nansum(y_values*norm_factor)
+
+        np.testing.assert_almost_equal(integral_sum, 1.0, decimal=3)
+
     def test_integrationUtils_same_scale(self):
 
         gauss2d = stats.multivariate_normal
