ad5fd18e9619665f662f1e857f5c13c8b6c919f7
Bryce Kalmbach <jbkalmbach@gmail.com>
2018-10-02 16:32:14 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="siggi",
-    version="0.1.0",
+    version="0.1.1",
     author="Bryce Kalmbach",
     author_email="brycek@uw.edu",
     url="https://github.com/jbkalmbach/siggi",
