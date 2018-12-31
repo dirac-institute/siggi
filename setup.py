196230de9687529292d609c08bf124bf066ba71c
Bryce Kalmbach <jbkalmbach@gmail.com>
2018-12-31 14:18:36 -0800

    Updating tag for automatic zenodo link.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="siggi",
-    version="0.1.4",
+    version="0.1.5",
     author="Bryce Kalmbach",
     author_email="brycek@uw.edu",
     url="https://github.com/jbkalmbach/siggi",
