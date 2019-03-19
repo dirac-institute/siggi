ceb76f7db8ef6a0b3435413cf05003bcd4824845
Bryce Kalmbach <jbkalmbach@gmail.com>
2019-03-18 21:15:49 -0700

    Testing 1-d case against KL divergence.

--- tests/test_calcIG.py
+++ tests/test_calcIG.py
@@ -1,4 +1,7 @@
 import unittest
+import sys
+sys.path.append('..')
+from scipy.stats import norm, entropy
 import numpy as np
 from siggi import filters, spectra, calcIG
 from siggi import Sed, Bandpass, BandpassDict
@@ -25,7 +28,12 @@ class testCalcIG(unittest.TestCase):
         cls.imsimBand = Bandpass()
         cls.imsimBand.imsimBandpass()
 
-        cls.phot_params = PhotometricParameters()
+        phot_params = {}
+        for idx in range(6):
+            phot_params['filter_%i' % idx] = PhotometricParameters(nexp=2,
+                sigmaSys=0.005, bandpass='filter_%i' % idx)
+
+        cls.phot_params = phot_params
 
     def test_calc_colors(self):
 
@@ -41,12 +49,13 @@ class testCalcIG(unittest.TestCase):
         norm_mag = np.random.uniform(21., 23.)
         for sed_obj in sed_list:
             sed_copy = deepcopy(sed_obj)
-            f_norm = sed_copy.calcFluxNorm(norm_mag, 
+            f_norm = sed_copy.calcFluxNorm(norm_mag,
                                            self.imsimBand)
             sed_copy.multiplyFluxNorm(f_norm)
             normed_sed_list.append(sed_copy)
 
-        test_c = calcIG(trap_dict, normed_sed_list, sed_probs)
+        test_c = calcIG(trap_dict, normed_sed_list, sed_probs,
+                        sky_mag=21.2, phot_params=self.phot_params)
 
         colors, errors = test_c.calc_colors()
 
@@ -59,7 +68,7 @@ class testCalcIG(unittest.TestCase):
                                       total_filt_dict['filter_0'],
                                       self.sky_spec,
                                       hardware_filt_dict['filter_0'],
-                                      self.phot_params, 1.0)
+                                      self.phot_params['filter_0'], 1.0)
 
         np.testing.assert_almost_equal(errors, [[test_error*np.sqrt(2)],
                                                 [test_error*np.sqrt(2)]])
@@ -89,8 +98,9 @@ class testCalcIG(unittest.TestCase):
         flambda_1[700:] *= f_norm_1_2
         sed_1.flambda = flambda_1
 
-        test_c2 = calcIG(trap_dict_2, [sed_1], [1.0],
-                         ref_filter=total_filt_dict_2['filter_0'])
+        test_c2 = calcIG(trap_dict_2, [sed_1], [1.0], sky_mag=21.2,
+                         ref_filter=total_filt_dict_2['filter_0'],
+                         phot_params=self.phot_params)
 
         colors2, errors2, snr2, mags2, sky_m2 = test_c2.calc_colors(
                                                         return_all=True)
@@ -155,6 +165,46 @@ class testCalcIG(unittest.TestCase):
                           sed_probs_2, sky_mag=20.0)
         ig_2 = test_c_2.calc_IG()
         self.assertAlmostEqual(ig_2, 2.0, delta=0.01)
+
+        # With one dimension should equal 1-d KL divergence
+        trap_dict_3 = self.f.trap_filters([[340., 370., 430., 460.],
+                                           [440., 470., 530., 560.]])
+        sed_probs_3 = [0.5, 0.5]
+
+        red_copy = deepcopy(self.red_spec)
+        f_norm = red_copy.calcFluxNorm(25.5, self.imsimBand)
+        red_copy.multiplyFluxNorm(f_norm)
+        red_copy_2 = deepcopy(self.blue_spec)
+        f_norm_2 = red_copy_2.calcFluxNorm(25., self.imsimBand)
+        red_copy_2.multiplyFluxNorm(f_norm_2)
+
+        sed_list_3 = [red_copy, red_copy_2]
+
+        test_c_3 = calcIG(trap_dict_3, sed_list_3,
+                          sed_probs_3)
+
+        ig = test_c_3.calc_IG()
+        colors, errors = test_c_3.calc_colors()
+        print(colors)
+        print(errors)
+
+        print(ig)
+
+        rv_1 = norm(loc=colors[0], scale=errors[0])
+        p1 = rv_1.pdf(np.arange(-2, 3, 0.001))
+        rv_2 = norm(loc=colors[1], scale=errors[1])
+        p2 = rv_2.pdf(np.arange(-2., 3, 0.001))
+        #rv_3 = norm(loc=mu_3, scale=sig_3)
+        #p3 = rv_3.pdf(np.arange(-3., 3, 0.001))
+        p3 = .5*p1 + .5*p2
+
+        print(entropy(p1, p3, base=2))
+        print(entropy(p2, p3, base=2))
+        print(.5*entropy(p1, p3, base=2) + .5*entropy(p2, p3, base=2))
+
+        kl_div = .5*entropy(p1, p3, base=2) + .5*entropy(p2, p3, base=2)
+
+        self.assertAlmostEqual(ig, kl_div, delta=0.01)
 
     @classmethod
     def tearDownClass(cls):
